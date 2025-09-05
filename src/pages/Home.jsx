57e80c92206d6263ddb8daae290a63ffdf3061da
Avanti <Avanti94Rachatte@gmail.com>
2025-09-05 16:50:25 +0530

    modify home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Carousel from '../components/Carousel'
 import { MidBanner } from '../components/MidBanner'
-import { Features } from '../components/Features'
+import  Features  from '../components/Features'
 
 
 
